d45b6a3121f1489346d0b36c5051256c1c301169
LosOleksandr <losalexander2003@gmail.com>
2023-10-04 19:45:58 +0300

    final

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,48 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {
+    signature = this.signature;
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    key = this.key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected tenants: Person[] = [];
+  protected door: boolean = false;
+  constructor(protected key: Key) {
+    key = this.key;
+  }
+
+  comeIn(person: Person) {
+    if (!this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log("door is open");
+    }
+  }
+}
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +50,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
